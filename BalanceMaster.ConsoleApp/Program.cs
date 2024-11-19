9d9bb5625b5c8e3863718ab1696aee66091f8392
davit.margvelashvili@gmail.com <davit.margvelashvili@gmail.com>
2024-11-19 22:28:47 +0400

    Console App Added

---
+++ BalanceMaster.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using BalanceMaster.Service.Commands;
+using BalanceMaster.Service.Services.Implementations;
+using System.Text.Json;
+
+namespace BalanceMaster.ConsoleApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var accountRepository = new InMemoryAccountRepository();
+        var operationService = new OperationService(accountRepository);
+
+        var json = File.ReadAllText("Data.json");
+
+        var debitCommands = JsonSerializer.Deserialize<List<DebitCommand>>(json);
+        foreach (var debitCommand in debitCommands)
+        {
+            operationService.Execute(debitCommand);
+        }
+    }
+}
