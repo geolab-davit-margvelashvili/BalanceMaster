0e30fae6d2e8a6bccbad8f7c8f3b714ab997a044
davit.margvelashvili@gmail.com <davit.margvelashvili@gmail.com>
2025-02-06 02:24:36 +0400

    Mail sender implemented

---
+++ BalanceMaster.MessageSender.Abstractions/Services/IEmailSender.cs
@@ -0,0 +1,6 @@
+﻿namespace BalanceMaster.MessageSender.Abstractions.Services;
+
+public interface IEmailSender
+{
+    Task SendEmailAsync(string email, string subject, string message);
+}
---
+++ BalanceMaster.MessageSender/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BalanceMaster.MessageSender.Abstractions.Models;
+using BalanceMaster.MessageSender.Abstractions.Services;
+using BalanceMaster.MessageSender.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BalanceMaster.MessageSender.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMailSender(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IEmailSender, EmailSender>();
+        services.Configure<EmailSenderOptions>(configuration.GetSection(EmailSenderOptions.Key));
+
+        return services;
+    }
+}
